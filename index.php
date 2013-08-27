84d4463acbb249b164b348b16fc760a5846d0117
Dorothy Tsai <dorothy.tsai@paperg.com>
2013-08-27 11:03:43 -0700

    index.php-modal for adding menu

--- index.php
+++ index.php
@@ -56,7 +56,27 @@ include 'authenticate.php';
                 echo "<td>" . $row['name'] . "</td>";
                 echo "<td>" . $row['food_type'] . "</td>";
                 echo "<td>" . ($row['menu_url'] ? "<button class=\"btn btn-info btn-xs\" onClick=\"window.open('" . $row['menu_url'] . "')\">View Menu</button>" :
-                        "<button id='add_menu' class=\"btn btn-default btn-xs\" onClick=alert(\"add menu!\")>Add Menu</button>") .
+                          "<!-- Button trigger modal -->
+                          <a data-toggle=\"modal\" href=\"#myModal\" class=\"btn btn-default btn-xs\">Add Menu</a>
+
+                          <!-- Modal -->
+                          <div class=\"modal fade\" id=\"myModal\" tabindex=\"-1\" role=\"dialog\" aria-labelledby=\"myModalLabel\" aria-hidden=\"true\">
+                            <div class=\"modal-dialog\">
+                              <div class=\"modal-content\">
+                                <div class=\"modal-header\">
+                                  <button type=\"button\" class=\"close\" data-dismiss=\"modal\" aria-hidden=\"true\">&times;</button>
+                                  <h4 class=\"modal-title\">Modal title</h4>
+                                </div>
+                                <div class=\"modal-body\">
+                                  ...
+                                </div>
+                                <div class=\"modal-footer\">
+                                  <button type=\"button\" class=\"btn btn-default\" data-dismiss=\"modal\">Close</button>
+                                  <button type=\"button\" class=\"btn btn-primary\">Save changes</button>
+                                </div>
+                              </div><!-- /.modal-content -->
+                            </div><!-- /.modal-dialog -->
+                          </div><!-- /.modal -->") .
                         " <button class=\"btn btn-success btn-xs\">Take Orders</button>" . "</td>";
                 echo "</tr>";
             }
